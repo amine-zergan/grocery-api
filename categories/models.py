eba26aee4959085bcc928aa70cb054148de7906c
amine-zergan <amine.mejri1000@gmail.com>
2023-06-08 19:04:10 +0100

    add gitignore with category model

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    id = models.IntegerField(primary_key=True,)
+    name=models.CharField(max_length=50,unique=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self) -> str:
+        return self.name
