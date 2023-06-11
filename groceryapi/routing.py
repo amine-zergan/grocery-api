ee486f6f7e1771dc0e9d77a4247314eed915c5b5
amine-zergan <amine.mejri1000@gmail.com>
2023-06-11 23:39:09 +0100

    mac-branch create

--- groceryapi/routing.py
+++ groceryapi/routing.py
@@ -1,7 +1,9 @@
-from .views import CategorieView
+from categories.views import CategorieView
+from stores.views import StoreView
 from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
 router.register(r'categories', CategorieView, basename='categorie')
+router.register(r'stories', StoreView, basename='stories')
 urlpatterns = router.urls
--- stores/urls.py
+++
@@ -1,8 +0,0 @@
- 
-from rest_framework.routers import DefaultRouter
-from .views import StoreView
-
-
-router = DefaultRouter()
-router.register(r'store', StoreView, basename='store')
-urlpatterns = router.urls
